717bfc885c63f4191f49966058f3dc39889915d1
Luan <luan.mgf@gmail.com>
2021-10-09 20:45:25 -0300

    novas mensagens

--- include/Messages.h
+++ include/Messages.h
@@ -2,7 +2,7 @@
  * @file Messages.h
  * @author Luan Mendes Gonçalves Freitas - 150015585
  * @brief Cabecalho de mensagens de textos
- * @version 0.3
+ * @version 0.4
  * 
  * @copyright Copyright (c) 2021
  * 
@@ -20,6 +20,8 @@
 #define WHITE	"\x1B[37m"
 #define RESET	"\x1B[0m"
 
+/** Game **/
+
 #define ERRO_INSTANCIA RED "Erro Instance nao instanciada" RESET
 
 #define ERRO_INIT_SDL RED "Erro SDL nao inicializada: " RESET
@@ -36,20 +38,44 @@
 
 #define ERRO_INIT_RENDERER RED "Erro Renderer nao inicializada: " RESET
 
+#define ERRO_CLEAR_RENDERER RED "Erro Renderer nao limpa: " RESET
+
+/** Sprite **/
+
 #define ERRO_INIT_TEXTURE RED "Erro Texture nao inicializada: " RESET
 
 #define ERRO_INIT_QUERY_TEXTURE RED "Erro Query Texture nao inicializada: " RESET
 
 #define ERRO_INIT_RENDER RED "Erro Render nao inicializada: " RESET
 
-#define ERRO_INIT_MUSIC RED "Erro Music nao carregada: " RESET
+/** Resources **/
+
+#define ERRO_OPEN_FILE_MUSIC RED "Erro nao foi possivel abrir arquivo music Mix_LoadMUS: " RESET
+
+#define ERRO_OPEN_FILE_SOUND RED "Erro nao foi possivel abrir arquivo sound Mix_LoadWAV: " RESET
+
+#define ERRO_OPEN_FILE_IMAGE RED "Erro nao foi possivel abrir arquivo image IMG_LoadTexture: " RESET
+
+#define ERRO_LOAD_FILE_TILEMAP RED "Erro nao foi possivel abrir arquivo tileMap IMG_LoadTexture: " RESET
+
+/** Music **/
+
+#define ERRO_PLAY_MUSIC RED "Erro de play musica music NULL: " RESET
+
+#define ERRO_STOP_MUSIC RED "Erro de stop musica music NULL: " RESET
+
+#define ERRO_PLAY_MIX_PLAYMUSIC RED "Erro de play musica no Mix_FadeOutMusic: " RESET
+
+#define ERRO_STOP_MIX_FADEOUTMUSIC RED "Erro de stop musica no Mix_FadeOutMusic: " RESET
+
+/** Sound **/
+
+#define ERRO_PLAY_SOUND RED "Erro de play som chunk NULL: " RESET
+
+#define ERRO_STOP_SOUND RED "Erro de stop som chunk NULL: " RESET
 
 #define ERRO_PLAY_MIX_PLAYCHANNEL RED "Erro de play som no Mix_PlayChannel: " RESET
 
 #define ERRO_STOP_MIX_HALTCHANNEL RED "Erro de stop som no Mix_HaltChannel: " RESET
-
-#define ERRO_OPEN_SOUND RED "Erro nao foi possivel abrir arquivo sound Mix_LoadWAV: " RESET
-
-#define ERRO_LOAD_FILE RED "Erro nao foi possivel carregar arquivo tileMap: " RESET
 
 #endif /* MESSAGES_H_ */
