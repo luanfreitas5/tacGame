1aa3f3036ff05bc6624ae4c5f554e87d7c949111
Luan <luan.mgf@gmail.com>
2021-10-09 21:13:14 -0300

    Classe CameraFollower

---
+++ include/CameraFollower.h
@@ -0,0 +1,66 @@
+/**
+ * @file CameraFollower.h
+ * @author Luan Mendes Gonçalves Freitas - 150015585
+ * @brief Cabecalho da classe CameraFollower com seus prototipos de metodo e ponteiros.
+ * @version 0.1
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#ifndef CAMERAFOLLOWER_H_
+#define CAMERAFOLLOWER_H_
+
+#include "Messages.h"
+#include "Camera.h"
+#include "GameObject.h"
+#include "Vec2.h"
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief Classe CameraFollower que armazenha um conjunto de blocos recortado de uma imagem para ser mostrado em um mapa de blocos.
+ * Herda metodos da classe Component
+ * 
+ */
+class CameraFollower: public Component {
+
+public:
+
+	/**
+	 * @brief Construtor da Classe CameraFollower
+	 * 
+	 * @param associated endereco do objeto de jogo ao qual o componente esta associado.
+	 */
+	CameraFollower(GameObject &associated);
+
+	/**
+	 * @brief Sobreposicao do metodo da classe Component.
+	 * Metodo para renderizar o componente.
+	 *
+	 */
+	void Render();
+
+	/**
+	 * @brief Sobreposicao do metodo da classe Component.
+	 * Metodo para atualizar o componente.
+	 *
+	 * @param dt valor Delta Time
+	 */
+	void Update(float dt);
+
+	/**
+	 * @brief Sobreposicao do metodo da classe Component.
+	 * Metodo para verificar o componente.
+	 *
+	 * @param type tipo de componente
+	 * @return true se o tipo componente for valido
+	 * @return false caso contrario
+	 */
+	bool Is(string type);
+
+private:
+};
+
+#endif /* CAMERAFOLLOWER_H_ */
---
+++ src/CameraFollower.cpp
@@ -0,0 +1,60 @@
+/**
+ * @file CameraFollower.cpp
+ * @author Luan Mendes Gonçalves Freitas - 150015585
+ * @brief Modulo dos metodos da classe CameraFollower
+ * @version 0.1
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include "../include/CameraFollower.h"
+
+/**
+ * @brief Construtor da Classe CameraFollower
+ *
+ * @param associated endereco do objeto de jogo ao qual o componente esta associado.
+ */
+CameraFollower::CameraFollower(GameObject &associated) :
+		Component(associated) {
+
+}
+
+/**
+ * @brief Sobreposicao do metodo da classe Component.
+ * Metodo para renderizar o componente.
+ *
+ */
+void CameraFollower::Render() {
+
+}
+
+/**
+ * @brief Sobreposicao do metodo da classe Component.
+ * Metodo para atualizar o componente.
+ *
+ * @param dt entrada de botoes do jogador
+ */
+void CameraFollower::Update(float dt) {
+
+	associated.box.x = Camera::pos.x;
+	associated.box.y = Camera::pos.y;
+
+}
+
+/**
+ * @brief Sobreposicao do metodo da classe Component.
+ * Metodo para verificar o componente.
+ *
+ * @param type tipo de componente
+ * @return true se o tipo componente for valido
+ * @return false caso contrario
+ */
+bool CameraFollower::Is(string type) {
+
+	if (type.compare("CameraFollower") == 0) {
+		return true;
+	}
+	return false;
+
+}
