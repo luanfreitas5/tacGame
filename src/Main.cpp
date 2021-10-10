54e36e8e3d92163ffc1c1ec6beda46539e7ed80f
Luan <luan.mgf@gmail.com>
2021-10-09 21:07:29 -0300

    main

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,18 +2,22 @@
  * @file Main.cpp
  * @author Luan Mendes Gonçalves Freitas - 150015585
  * @brief Modulo de inicializacao do programa/jogo;
- * @version 0.2
+ * @version 0.3
  * 
  * @copyright Copyright (c) 2021
  * 
  */
 #include "../include/Game.h"
 #include "../include/Resources.h"
+#include "../include/Camera.h"
 
 Game *Game::instance = nullptr;
 unordered_map<string, SDL_Texture*> Resources::imageTable = { { "", nullptr } };
 unordered_map<string, Mix_Music*> Resources::musicTable = { { "", nullptr } };
 unordered_map<string, Mix_Chunk*> Resources::soundTable = { { "", nullptr } };
+GameObject *Camera::focus = nullptr;
+Vec2 Camera::pos = Vec2();
+Vec2 Camera::speed = Vec2(SPEED_X, SPEED_Y);
 
 /**
  * @brief Metodo de inicializacao do programa/jogo
