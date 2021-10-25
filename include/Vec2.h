ac859f47ea74fb1eaa88603107f030fe09deeaf8
Luan <luan.mgf@gmail.com>
2021-10-24 22:37:06 -0300

    Vec2

--- include/Vec2.h
+++ include/Vec2.h
@@ -2,7 +2,7 @@
  * @file Vec2.h
  * @author Luan Mendes Gonçalves Freitas - 150015585
  * @brief Cabecalho da classe Vec2 com seus prototipos de metodo e ponteiros.
- * @version 0.1
+ * @version 0.2
  * 
  * @copyright Copyright (c) 2021
  * 
@@ -49,6 +49,134 @@ public:
 	 * @param distancia valor da distancia entre o primeiro valor Vec2 e o segundo 2 Vec2 aleatorio.
 	 */
 	void CalcularRotacaoAngulo(float distancia);
+
+	/**
+	 * @brief Operacao Adicao
+	 * 
+	 * @param v 
+	 * @return Vec2 
+	 */
+	Vec2 operator+(const Vec2 &v);
+
+	/**
+	 * @brief Operacao Adicao
+	 * 
+	 * @param v 
+	 * @return Vec2 
+	 */
+	Vec2 operator+(const float v);
+
+	/**
+	 * @brief Operacao Substracao
+	 * 
+	 * @param v 
+	 * @return Vec2 
+	 */
+	Vec2 operator-(const Vec2 &v);
+
+	/**
+	 * @brief Operacao Substracao
+	 * 
+	 * @param v 
+	 * @return Vec2 
+	 */
+	Vec2 operator-(const float v);
+
+	/**
+	 * @brief Operacao Multiplicacao
+	 * 
+	 * @param v 
+	 * @return Vec2 
+	 */
+	Vec2 operator*(const Vec2 &v);
+
+	/**
+	 * @brief Operacao Multiplicacao
+	 * 
+	 * @param v 
+	 * @return Vec2 
+	 */
+	Vec2 operator*(const float v);
+
+	/**
+	 * @brief Operacao Divisao
+	 * 
+	 * @param v 
+	 * @return Vec2 
+	 */
+	Vec2 operator/(const Vec2 &v);
+
+	/**
+	 * @brief Operacao Divisao
+	 * 
+	 * @param v 
+	 * @return Vec2 
+	 */
+	Vec2 operator/(const float v);
+
+	/**
+	 * @brief Get Distance
+	 * 
+	 * @param vec2 
+	 * @return float 
+	 */
+	float GetDistance(Vec2 vec2);
+
+	/**
+	 * @brief Get Distance X
+	 * 
+	 * @param x 
+	 * @return float 
+	 */
+	float GetDistanceX(float x);
+
+	/**
+	 * @brief Get Distance Y
+	 * 
+	 * @param y 
+	 * @return float 
+	 */
+	float GetDistanceY(float y);
+
+	/**
+	 * @brief Calcular Cos
+	 * 
+	 * @param angle 
+	 * @return float 
+	 */
+	static float Cos(float angle);
+
+	/**
+	 * @brief Get valor Cos
+	 * 
+	 * @param vec2 
+	 * @return float 
+	 */
+	float GetCos(Vec2 vec2);
+
+	/**
+	 * @brief Calcular Sin
+	 * 
+	 * @param angle 
+	 * @return float 
+	 */
+	static float Sin(float angle);
+
+	/**
+	 * @brief Get valor Sin
+	 * 
+	 * @param vec2 
+	 * @return float 
+	 */
+	float GetSin(Vec2 vec2);
+
+	/**
+	 * @brief Get Angle
+	 * 
+	 * @param vec2 
+	 * @return float 
+	 */
+	float GetAngle(Vec2 vec2);
 
 	/**
 	 * @brief coordenada horizontal do retangulo
--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -2,7 +2,7 @@
  * @file Vec2.cpp
  * @author Luan Mendes Gonçalves Freitas - 150015585
  * @brief Modulo dos metodos da classe Vec2
- * @version 0.1
+ * @version 0.2
  * 
  * @copyright Copyright (c) 2021
  * 
@@ -46,4 +46,224 @@ void Vec2::CalcularRotacaoAngulo(float distancia) {
 
 	x += (float) (cos(angulo) * distancia);
 	y += (float) (sin(angulo) * distancia);
+}
+
+/**
+ * @brief Operacao Adicao
+ *
+ * @param v
+ * @return Vec2
+ */
+Vec2 Vec2::operator+(const Vec2 &v) {
+
+	Vec2 vec2 = Vec2(x + v.x, y + v.y);
+	return vec2;
+
+}
+
+/**
+ * @brief Operacao Adicao
+ *
+ * @param v
+ * @return Vec2
+ */
+Vec2 Vec2::operator+(const float v) {
+
+	Vec2 vec2 = Vec2(x + v, y + v);
+	return vec2;
+
+}
+
+/**
+ * @brief Operacao Substracao
+ *
+ * @param v
+ * @return Vec2
+ */
+Vec2 Vec2::operator-(const Vec2 &v) {
+
+	Vec2 vec2 = Vec2(x - v.x, y - v.y);
+	return vec2;
+
+}
+
+Vec2 Vec2::operator-(const float v) {
+
+	Vec2 vec2 = Vec2(x - v, y - v);
+	return vec2;
+
+}
+
+/**
+ * @brief Operacao Multiplicacao
+ *
+ * @param v
+ * @return Vec2
+ */
+Vec2 Vec2::operator*(const Vec2 &v) {
+
+	Vec2 vec2 = Vec2(x * v.x, y * v.y);
+	return vec2;
+
+}
+
+/**
+ * @brief Operacao Multiplicacao
+ *
+ * @param v
+ * @return Vec2
+ */
+Vec2 Vec2::operator*(const float v) {
+
+	Vec2 vec2 = Vec2(x * v, y * v);
+	return vec2;
+
+}
+
+/**
+ * @brief Operacao Divisao
+ *
+ * @param v
+ * @return Vec2
+ */
+Vec2 Vec2::operator/(const Vec2 &v) {
+
+	Vec2 vec2 = Vec2(x / v.x, y / v.y);
+	return vec2;
+
+}
+
+/**
+ * @brief Operacao Divisao
+ *
+ * @param v
+ * @return Vec2
+ */
+Vec2 Vec2::operator/(const float v) {
+
+	Vec2 vec2 = Vec2(x / v, y / v);
+	return vec2;
+
+}
+
+/**
+ * @brief Get Distance
+ *
+ * @param vec2
+ * @return float
+ */
+float Vec2::GetDistance(Vec2 vec2) {
+
+	float x = pow(GetDistanceX(vec2.x), 2);
+	float y = pow(GetDistanceY(vec2.y), 2);
+
+	float distance = pow(x + y, 0.5);
+
+	return distance;
+
+}
+
+/**
+ * @brief Get Distance X
+ *
+ * @param x
+ * @return float
+ */
+float Vec2::GetDistanceX(float x) {
+
+	float distance = x - this->x;
+
+	return distance;
+
+}
+
+/**
+ * @brief Get Distance Y
+ *
+ * @param y
+ * @return float
+ */
+float Vec2::GetDistanceY(float y) {
+
+	float distance = y - this->y;
+
+	return distance;
+
+}
+
+/**
+ * @brief Calcular Cos
+ *
+ * @param angle
+ * @return float
+ */
+float Vec2::Cos(float angle) {
+
+	float cos_angle = cos(angle * PI / 180);
+
+	return cos_angle;
+
+}
+
+/**
+ * @brief Get valor Cos
+ *
+ * @param vec2
+ * @return float
+ */
+float Vec2::GetCos(Vec2 vec2) {
+
+	float x = GetDistanceX(vec2.x);
+	float y = GetDistance(vec2);
+
+	float valor_sin = x / y;
+
+	return valor_sin;
+
+}
+
+/**
+ * @brief Calcular Sin
+ *
+ * @param angle
+ * @return float
+ */
+float Vec2::Sin(float angle) {
+
+	float sin_angle = sin(angle * PI / 180);
+
+	return sin_angle;
+
+}
+
+/**
+ * @brief Get valor Sin
+ *
+ * @param vec2
+ * @return float
+ */
+float Vec2::GetSin(Vec2 vec2) {
+	float x = GetDistance(vec2);
+	float y = GetDistanceY(vec2.y);
+
+	float valor_sin = y / x;
+
+	return valor_sin;
+
+}
+
+/**
+ * @brief Get Angle
+ *
+ * @param vec2
+ * @return float
+ */
+float Vec2::GetAngle(Vec2 vec2) {
+
+	float x = GetDistanceX(vec2.x);
+	float y = GetDistanceY(vec2.y);
+	float angle = atan2(y, x) * (180 / PI);
+
+	return angle;
+
 }
